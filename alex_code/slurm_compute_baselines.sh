6c997de8db1316d3379a50bfab5d6c420ef60783
alexadam <paddze21@gmail.com>
2019-03-29 10:42:02 -0400

    Added individual heuristic and timeout to baseline computations
    -One can now simply run the slurm_compute_baselines.sh by calling
    sbatch slurm_compute_baselines.sh. Make sure to change the conda
    path and environment name in the script where indicated.

--- alex_code/slurm_compute_baselines.sh
+++ alex_code/slurm_compute_baselines.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=4
-#SBATCH --ntasks=2
-#SBATCH --mem-per-cpu=7000
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=2000
 
 
 
@@ -11,6 +11,7 @@ cd ..
 source add_python_path.sh
 cd alex_code
 
+# Replace with your conda path and name of your conda env
 source /h/alexadam/anaconda3/bin/activate gnn
 
 domain=logistics
@@ -18,25 +19,34 @@ competition=38
 problems=$(ls ../pddl_files/${domain}/${competition})
 
 count=0
+heuristics=(hadd lmcut blind)
+time_limits=(30  60 90)
 
-for problem in ${problems[@]}
+for time_limit in ${time_limits[@]}
 do
-    if [ "$problem" == "domain.pddl" ]
-    then
-        continue
-    fi
+    for heuristic in ${heuristics[@]}
+    do
+        for problem in ${problems[@]}
+        do
+            if [ "$problem" == "domain.pddl" ]
+            then
+                continue
+            fi
 
-    echo $problem
+            echo $problem
 
-    domain_file="${domain}/${competition}/domain.pddl"
-    problem_file="${domain}/${competition}/${problem}"
+            domain_file="${domain}/${competition}/domain.pddl"
+            problem_file="${domain}/${competition}/${problem}"
 
-    python -u compute_baselines.py --domain-file=$domain_file --problem-file=$problem_file &
-    count=$(( count + 1 ))
+            python -u compute_baselines.py --domain-file=$domain_file --problem-file=$problem_file --heuristic=$heuristic --time-limit=$time_limit &
+            count=$(( count + 1 ))
 
-    if [ ${count} -gt 1 ]
-    then
-        count=0
-        wait
-    fi
+            if [ ${count} -gt 3 ]
+            then
+                count=0
+                wait
+            fi
+        done
+    done
 done
+
