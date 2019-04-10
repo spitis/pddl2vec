fc1fe136c13cd14f599ef60a6dbb6962a9981fb1
eleniTriantafillou <eleni.triantafillou@mail.utoronto.ca>
2019-04-10 17:55:52 -0400

    comparing RL heuristic with standard ones

--- search_value_heuristic.py
+++ search_value_heuristic.py
@@ -4,7 +4,7 @@ import timeit
 import numpy as np
 import tensorflow as tf
 from argparse import ArgumentParser
-from pyperplan import search
+from pyperplan import search, get_heuristics, _get_heuristic_name
 
 from pddl2env import PddlBasicEnv
 from value_iteration_sp import SimpleValueIteration
@@ -34,17 +34,9 @@ def wrapper(func, *args, **kwargs):
     return wrapped
 
 
-def evaluate(vi_model):
-    """Solve a number of problems."""
-    # Make sure the current state is the initial state.
-    vi_model.env.reset()
-
-    # The heuristic estimate of each given state based on the model's values.
-    heuristic = ValueIterationHeuristic(vi_model._obs_ph, vi_model.values,
-                                        vi_model.sess, vi_model.env)
-   
-    # Perform the search using the value-iteration-learned heuristic.
-    do_search = wrapper(search.astar_search, vi_model.env.task, heuristic)
+def perform_search(task, heuristic):
+    """Runs A-star search in the given task using the given heuristic."""
+    do_search = wrapper(search.astar_search, task, heuristic)
     solution, expansions = do_search()
     time_taken = timeit.timeit(do_search, number=1)
 
@@ -57,12 +49,30 @@ def evaluate(vi_model):
     }
 
 
+def evaluate_vi_model(vi_model):
+    """Performs search using the given value-iteration model."""
+    # Make sure the current state is the initial state.
+    vi_model.env.reset()
+
+    # The heuristic estimate of each given state based on the model's values.
+    heuristic = ValueIterationHeuristic(vi_model._obs_ph, vi_model.values,
+                                        vi_model.sess, vi_model.env)
+    return perform_search(vi_model.env.task, heuristic)
+
+
+def evaluate_standard(env, heuristic):
+    """Performs search using the given standard heuristic."""
+    # Make sure the current state is the initial state.
+    env.reset()
+    return perform_search(env.task, heuristic)
+
+
 def main(args):
     problem_name = os.path.basename(args.problem_file).split(".")[0]
-    
+
     # Get an environment for the specified domain and problem.
     env = PddlBasicEnv(domain=args.domain_file, instance=args.problem_file)
-  
+
     # Load a pre-trained Value-Iteration model.
     vi_model = SimpleValueIteration(
         env=env, ckpt_dir=args.restore_dir, restore_dir=args.restore_dir)
@@ -71,9 +81,20 @@ def main(args):
     vi_model_random = SimpleValueIteration(env=env)
 
     results = {}
-    results["vi_heuristic"] = evaluate(vi_model) 
-    results["vi_heuristic_random_weights"] = evaluate(vi_model_random)
-    print(results)
+    results["vi_heuristic"] = evaluate_vi_model(vi_model)
+    results["vi_heuristic_random_weights"] = evaluate_vi_model(vi_model_random)
+
+    # Also try some standard heuristics on the same problem.
+    heuristics = {_get_heuristic_name(h): h for h in get_heuristics()}
+    for h_name, h in heuristics.items():
+        if h_name in ['valueiteration', 'node2vec']:
+          continue
+        results[h_name] = evaluate_standard(env, h(env.task))
+
+    # Print the results of all heuristics.
+    for model, model_results in results.items():
+        print("model {}: {}".format(model, model_results))
+
 
 if __name__ == "__main__":
     main(parser.parse_args())
